0305597f67cdd863423b069ca3855fc563c06e0c
Harrison Luff <78385627+harrisonluff@users.noreply.github.com>
2024-08-13 16:09:48 +0100

    Implemented solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    function test(n) {
+        if (!(typeof n == 'number')) {
+            return false
+        }
+        if (n < 0) {
+            return false
+        }
+        if (!(n == Math.floor(n))) {
+            return false
+        }
+        return true
+    };
+
+    if (!test(a) || !test(b)) {
+        return 'ERROR'
+    }
+
+    let low;
+    let high;
+    if (a <= b) {
+        low = a;
+        high = b;
+    } else {
+        low = b;
+        high = a;
+    }
+
+    let sum = 0;
+    for (let i=low; i<=high; i++) {
+        sum += i;
+    }
+    return sum
 
 };
 
