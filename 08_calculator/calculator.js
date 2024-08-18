436edfc8509e424a40f37b6afa71580eecb18389
Harrison Luff <78385627+harrisonluff@users.noreply.github.com>
2024-08-18 15:20:53 +0100

    implemented solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(((total, current) => total += current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  // Assumes non-empty array
+  return arr.reduce(((total, current) => total *= current), 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	out = 1;
+  for (let i=0; i<b; i++) {
+    out *= a;
+  }
+  return out;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	out = 1;
+  for (let i=1; i<=a; i++) {
+    out *= i;
+  }
+  return out
 };
 
 // Do not edit below this line
