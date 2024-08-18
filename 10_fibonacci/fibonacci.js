8528f98da07d307243917ae0cf363219928f9341
Harrison Luff <78385627+harrisonluff@users.noreply.github.com>
2024-08-18 15:46:58 +0100

    implemented solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) return 'OOPS';
+    if (n == 0) return 0;
 
+    a = 1; //current
+    b = 0; //last
+
+    for (let i=0; i<n-1; i++) {
+        [a, b] = [a+b, a];
+    }
+    return a;
 };
 
 // Do not edit below this line
