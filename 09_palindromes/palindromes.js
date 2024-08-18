5f8b00b37507eb96973115d5bed4bc86cb9108b3
Harrison Luff <78385627+harrisonluff@users.noreply.github.com>
2024-08-18 15:35:52 +0100

    implemented solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+    letters = str.toLowerCase().split('').filter(char => alphabet.includes(char));
+    reversed = letters.slice().reverse();
+    return letters.toString() == reversed.toString();
 };
 
 // Do not edit below this line
